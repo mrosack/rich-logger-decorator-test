6b38bfc30c4ec4b2d080ea86907e978b959d401e
Mike Rosack <mrosack@gmail.com>
2017-08-31 14:54:44 -0500

    try to add ClassLogger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { ClassLogger } from "rich-logger-decorator";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@ClassLogger()
 export class AppComponent {
   title = 'app';
 }
